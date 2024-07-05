c32a6d7e95dca51df502ca94bfc85793de802f00
Shashank <shashanknegi@pearlorganisation.com>
2024-07-05 11:14:38 +0530

    added about us

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/_components/Footer/Footer";
+import Header from "@/_components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        
+        <Footer/>
+        </body>
     </html>
   );
 }
